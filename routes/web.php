1a2e38f7803c2238416ebc7aca875cc889801529
adesupriono520 <adesupriono2@gmail.com>
2024-09-23 10:28:06 +0700

    Modul Auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/login', [AuthController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
